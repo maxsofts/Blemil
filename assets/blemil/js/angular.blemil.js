357b5c1519be84a92fd1f88a73abec3eafad9503
Tran Xuan Duc (Trần Xuân Đức) <ductranxuan.29710@gmail.com>
2016-07-25 15:09:15 +0700

    Update angularjs

--- assets/blemil/js/angular.blemil.js
+++ assets/blemil/js/angular.blemil.js
@@ -1,1 +1,86 @@
-var app = angular.module('blemilApp', []);
+var app = angular.module('blemilApp', ['ngRoute']);
+/**
+ * Router
+ */
+app.config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
+        $routeProvider
+            .when('/', {
+                templateUrl: 'inc/pages/home.html',
+                controller: 'HomeController',
+                title: 'Trang chủ'
+            })
+            .when('/about', {
+                templateUrl: 'inc/pages/about.html',
+                controller: 'AboutController',
+                title: 'Giới thiệu'
+            })
+            .when('/contact', {
+                templateUrl: 'inc/pages/contact.html',
+                controller: 'ContactController',
+                title: 'Liên hệ'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+app.run(['$location', '$rootScope', function ($location, $rootScope) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+
+        if (current.hasOwnProperty('$$route')) {
+
+            $rootScope.title = current.$$route.title;
+        }
+    });
+}]);
+/**
+ * Controller
+ */
+//Home
+app.controller('HomeController', function ($scope) {
+    //$scope.title = "Trang chủ";
+    $scope.data = "Ở xin chào đây là trang chủ";
+});
+//About us
+app.controller('AboutController', function ($scope) {
+    //$scope.title = "Giới thiệu";
+    $scope.data = "Ở xin chào đây trang about us";
+});
+app.controller('ContactController', function ($scope) {
+    //$scope.title = "Liên hệ";
+    $scope.data = "Ở xin chào đây trang liên hệ";
+});
+
+
+/**
+ * Component
+ */
+//Header
+app.component('comHeader', {
+    'templateUrl': 'inc/modules/header.html',
+    controller: function blemil_header() {
+        this.menus = [
+            {
+                name: "Trang chủ",
+                link: "/#!"
+            },
+            {
+                name: "Giới thiệu",
+                link: "#!/about"
+            },
+            {
+                name: "Liên hệ",
+                link: "#!/contact"
+            }
+        ]
+    }
+});
+//Footer
+app.component('comFooter', {
+    'templateUrl': 'inc/modules/footer.html',
+    controller: function blemil_footer() {
+
+    }
+});
